0c2dcf814f98ed4011bf0f2cd3e8cbea7be9b359
tyrrannt <62670631+tyrrannt@users.noreply.github.com>
2021-01-17 12:54:19 +0300

    lesson 3

--- js/main.js
+++ js/main.js
@@ -1,21 +1,84 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
-    { id: 3, title: 'Keyboard', price: 200 },
-    { id: 4, title: 'Gamepad', price: 50 },
-];
-//Функция для формирования верстки каждого товара
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3 class="product-item-h3">${title}</h3>
-                <p class="product-item-p">${price}</p>
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+class ProductDB {
+    fetchProducts() {
+        return fetch(`${API_URL}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log('error');
+            })
+    }
+}
+
+
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this.totalProducts = [];
+        this._fetchProducts()
+            .then(data => {
+                this.goods = [...data];
+                this.render()
+            });
+    }
+
+    _fetchProducts() {
+        return fetch(`${API_URL}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log('error');
+            })
+    }
+    render() {
+        const block = document.querySelector(this.container);
+
+        for (let product of this.goods) {
+            const productObj = new ProductItem(product);
+            this.totalProducts.push(productObj);
+            block.insertAdjacentHTML('beforeend', productObj.render())
+        }
+
+    }
+    getSum() {
+        let result = this.totalProducts.reduce((sum, item) => sum += item.price, 0);
+        const block = document.querySelector('.top-box-button');
+        block.insertAdjacentHTML('beforeend', ': ' + result + ' руб.');
+    }
+}
+
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = `img/${product.id_product}.jpg`;
+
+    }
+
+    render() {
+        return `<div class="product-item  data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <h3 class="product-item-h3">${this.title}</h3>
+                <p class="product-item-p">${this.price}</p>
                 <button class="product-item-buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    // const productsList = list.map(item => renderProduct(item.title, item.price)).join('');
-    // console.log(productsList);
-    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price)).join('');
-};
+    }
+}
 
-renderPage(products);
+let list = new ProductsList();
+list.render();
+
+
+class Baskets {
+    add_good() {
+
+    }
+    remove_good() {
+
+    }
+}
+
+class BasketsItem {
+
+}
