2015572f763a26ce06adad20eb8b7e8b8d70aef3
tyrrannt <62670631+tyrrannt@users.noreply.github.com>
2021-01-24 01:13:39 +0300

    4 урок

--- js/main.js
+++ js/main.js
@@ -1,21 +1,36 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
-    { id: 3, title: 'Keyboard', price: 200 },
-    { id: 4, title: 'Gamepad', price: 50 },
-];
-//Функция для формирования верстки каждого товара
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <h3 class="product-item-h3">${title}</h3>
-                <p class="product-item-p">${price}</p>
-                <button class="product-item-buy-btn">Купить</button>
-            </div>`
-};
-const renderPage = list => {
-    // const productsList = list.map(item => renderProduct(item.title, item.price)).join('');
-    // console.log(productsList);
-    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price)).join('');
-};
+function regxpText() {
+    document.querySelectorAll(".src").forEach(elem => {
+        let str = elem.innerHTML;
+        let regexpAllPoints = new RegExp('\'', 'gm');
+        let regexpReturnApostroph = /([a-zа-яё])\"([a-zа-яё])/gmi;
+        let newstr = str.replace(regexpAllPoints, '"');
+        newstr = newstr.replace(regexpReturnApostroph, '$1\'$2');
+        elem.innerHTML = newstr;
+    })
+}
+document.getElementById('check').addEventListener("click", regxpText);
 
-renderPage(products);
+function check(elem, event) {
+    for (let el of elem) {
+        if (el[1].match(el[2])) {
+            field = document.getElementsByName(el[0])[0];
+            field.classList.replace('invalid', 'valid');
+        }
+        else {
+            field = document.getElementsByName(el[0])[0];
+            field.classList.replace('valid', 'invalid');
+            event.preventDefault();
+        }
+    }
+}
+
+function valideForm(event) {
+    let elem = [
+        ['name', document.getElementsByName('name')[0].value, /^[a-zа-яё]+$/gi],
+        ['email', document.getElementsByName('email')[0].value, /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/],
+        ['phone', document.getElementsByName('phone')[0].value, /^\+\d{1,3}\s?\(\d{3}\)\s?\d{3}(-\d{2}){2}$/]
+    ]
+    check(elem, event)
+    
+}
+document.getElementById('checkMe').addEventListener("click", valideForm);
